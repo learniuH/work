7fbb10ff59f6766efb4e9317c3d1541553984414
learniuH <166969027+learniuH@users.noreply.github.com>
2024-11-04 21:00:01 +0800

    Add files via upload

--- KCTS/read_file.py
+++ KCTS/read_file.py
@@ -1,87 +1,103 @@
-from openpyxl import load_workbook
-import re
+import pandas as pd
 
-# 加载 Excel 文件
-workbook = load_workbook(filename="test.xlsx")
-# 查看 OU->MU 表单
-ou_to_mu = workbook['OU->MU']
+def clean_bit_number(bit_index):
+    ''' 从 bit_index 中提取数字 '''
+    if isinstance(bit_index, (int, float)):
+        # 如果是数字直接 return
+        return bit_index
+    if 'bit' in bit_index.lower():
+        # 处理 bit0 等格式
+        bit_index = bit_index.lower().replace('bit', '')
+        if bit_index.isdigit():
+            # 返回 int
+            return int(bit_index)
+        # 处理 byte2-3 等范围格式
+        if '-' in bit_index:
+            # 返回 str
+            return bit_index
+    return bit_index
 
-# 初始化协议字典
-ou_protocol = {}
+def clean_byte_number(byte_num):
+    ''' 从 byte_num 中提取数字 '''
+    if isinstance(byte_num, (int, float)):
+        # 如果是数字直接 return
+        return byte_num
+    if 'byte' in byte_num.lower():
+        # 处理 byte1 等格式
+        byte_num = byte_num.lower().replace('byte', '')
+        if byte_num.isdigit():
+            # 返回 int
+            return int(byte_num)
+        # 处理 byte2-3 等范围格式
+        if '-' in byte_num:
+            # 返回 str
+            return byte_num
+    return byte_num
 
-# 遍历所有合并的单元格
-for merged_range in ou_to_mu.merged_cells.ranges:
-    # 合并单元格的开始和终止坐标
-    start_idx_str, end_idx_str = merged_range.coord.split(':')
-    # 合并单元格的起始和终止单元格
-    start_cell, end_cell = ou_to_mu[start_idx_str], ou_to_mu[end_idx_str]
-    if start_cell.value == '开关量':
-        # 遍历开关量的字节序号
-        byte_col = start_cell.column + 1
-        for byte_row in range(start_cell.row, end_cell.row + 1):
-            # 合并的字节序号如果为空就跳过
-            if ou_to_mu.cell(row=byte_row, column=byte_col).value:
-                # 获取开关量的字节序号
-                if isinstance(ou_to_mu.cell(row=byte_row, column=byte_col).value, str):
-                    byte_num = int(re.findall(r'\d', ou_to_mu.cell(row=byte_row, column=byte_col).value)[0])
-                else:
-                    byte_num = ou_to_mu.cell(row=byte_row, column=byte_col).value
-                # 更新协议的字节号
-                ou_protocol.update({byte_num:{}})
-                # 将字节序号的行列转换为坐标形式 A1
-                byte_coord = ou_to_mu.cell(row=byte_row, column=byte_col).coordinate
-                # 检查字节序号是合并的单元格
-                is_merged = None
-                for is_merged in ou_to_mu.merged_cells.ranges:
-                    if byte_coord in is_merged:
-                        # 如果是合并的单元格就更新坐标
-                        byte_coord = is_merged.coord
-                        # 获取字节序号合并单元格的开始和终止坐标
-                        start_byte_idx, end_byte_idx = byte_coord.split(':')
-                        # 字节序号起始和终止的单元格
-                        start_byte_cell, end_byte_cell = ou_to_mu[start_byte_idx], ou_to_mu[end_byte_idx]
-                        # 遍历对应字节的bit位
-                        bit_col = start_byte_cell.column + 1
-                        for bit_row in range(start_byte_cell.row, end_byte_cell.row + 1):
-                            # 获取bit位的索引
-                            if isinstance(ou_to_mu.cell(row=bit_col, column=bit_col).value, str):
-                                bit_index = int(re.findall(r'\d', ou_to_mu.cell(row=bit_row, column=bit_col).value)[0])
-                            else:
-                                bit_index = ou_to_mu.cell(row=bit_row, column=bit_col).value
-                            # 获取开关量的协议内容(规定bit内容的列数在bit位右边一列)
-                            funciton_col = bit_col + 1
-                            bit_function = ou_to_mu.cell(row=bit_row, column=funciton_col).value
-                            # 更新协议字典
-                            ou_protocol.get(byte_num).update({bit_index: bit_function})
-                    # 如果开关量的字节序号不是合并单元格
-                    # else:
-                    #     bit_row, bit_col, function_col = byte_row, byte_col + 1, byte_col + 2
-                    #     print(f'现在的行是{bit_row}现在的列是{byte_col}')
-                    #     # 获取bit位索引
-                    #     if isinstance(ou_to_mu.cell(row=bit_row, column=bit_col).value, str):
-                    #         bit_index = int(re.findall(r'\d', ou_to_mu.cell(row=bit_row, column=bit_col).value)[0])
-                    #     else:
-                    #         bit_index = ou_to_mu.cell(row=bit_row, column=bit_col).value
-                    #     bit_function = ou_to_mu.cell(row=bit_col, column=funciton_col).value
-                    #     # 更新协议字典
-                    #     ou_protocol.get(byte_num).update({bit_index: bit_function})
+def read_protocol_excel(file_path, sheet_name):
+    ou_to_mu_df = pd.read_excel(file_path, sheet_name=sheet_name, header=1)
 
-    elif start_cell.value == '模拟量':
-        # 遍历模拟量的字节序号
-        byte_col = start_cell.column + 1
-        for byte_row in range(start_cell.row, end_cell.row + 1):
-            # 获取模拟量的字节序号
-            if isinstance(ou_to_mu.cell(row=byte_row, column=byte_col).value, str):
-                byte_num = int(re.findall(r'\d', ou_to_mu.cell(row=byte_row, column=byte_col).value)[0])
-            else:
-                byte_num = ou_to_mu.cell(row=byte_row, column=byte_col).value
-            # 获取模拟量的协议内容(规定字节内容的列数为字节序号 + 2)
-            funciton_col = byte_col + 2
-            byte_function = ou_to_mu.cell(row=byte_row, column=funciton_col).value
-            # 更新协议字典
-            if byte_function == '预留':
-                continue
-            else:
-                ou_protocol.update({byte_num: byte_function})
 
-print(ou_protocol)
+    # # 筛选 '字节序号' 所在行的索引
+    # header = ou_to_mu_df[ou_to_mu_df.eq('字节序号').any(axis=1)].index[0]
+    # ou_to_mu_df.columns[header]
+
+
+    # 将名称和字节序号两列填充
+    ou_to_mu_df.loc[:, '名称'] = ou_to_mu_df['名称'].fillna(method='ffill')
+    ou_to_mu_df.loc[:, '字节序号'] = ou_to_mu_df['字节序号'].fillna(method='ffill')
+
+    # 修改名称这列为 index
+    ou_to_mu_df.set_index('名称', inplace=True)
+
+    # 初始化协议字典
+    ou_protocol = {}
+
+    # 取开关量和模拟量这部分的 df
+    switch_df = ou_to_mu_df.loc['开关量', :]
+    analog_df = ou_to_mu_df.loc['模拟量', :]
+
+    # 开关量协议更新
+    for _, switch_row in switch_df.iterrows():
+        # 通过 '字节序号' 索引读取 byte_num
+        byte_num = switch_row['字节序号']
+        # 提取 byte_num 中的数字
+        byte_num = clean_byte_number(byte_num)
+        # 更新协议
+        if byte_num not in ou_protocol:
+            ou_protocol[byte_num] = {}
+
+        # 通过 '内容' 索引读取 bit_index
+        bit_index = switch_row['内容']
+        # 提取 bit_index 中的数字
+        bit_index = clean_bit_number(bit_index)
+
+        # 通过 '描述' 更新
+        switch_desc = switch_row['描述']
+        # 更新协议
+        if switch_desc != '预留' and not pd.isnull(switch_desc):
+            ou_protocol[byte_num][bit_index] = switch_desc
+        else:   # 删除预留和空白的字节序号
+            ou_protocol.pop(byte_num)
+
+    # 模拟量协议更新
+    for _, analog_row in analog_df.iterrows():
+        # 通过 '字节序号' 索引读取 byte_num
+        byte_num = analog_row['字节序号']
+        # 提取 byte_num 中的数字
+        byte_num = clean_byte_number(byte_num)
+
+        # 通过 '描述' 更新
+        analog_desc = analog_row['描述']
+        # 更新协议
+        if analog_desc != '预留' and not pd.isnull(analog_desc):
+            ou_protocol[byte_num] = analog_desc
+
+    return ou_protocol
+
+
+if __name__ == '__main__':
+    file_path = './金冠铜业-双闪配料行车KC144-CRB-JG-220725-通信协议设计-洪常乐-双车20240603.xls'
+    sheet_name = 'OU->MU'
+    ou_protocol = read_protocol_excel(file_path, sheet_name)
+    print(ou_protocol)
