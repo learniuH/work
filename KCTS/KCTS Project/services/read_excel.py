c7e68e66a9a11abea5b54c38c3141c20da30c978
learniuH <L13320500248@163.com>
2024-11-26 00:13:03 +0800

    更新读取协议内容

--- KCTS/KCTS Project/services/read_excel.py
+++ KCTS/KCTS Project/services/read_excel.py
@@ -1,8 +1,9 @@
+
 import pandas as pd
 
 
 class ExcelRead():
-    def __init__(self, file_path):
+    def __init__(self, file_path: str):
         # 表单的名字
         self.file_path = file_path
 
@@ -14,45 +15,55 @@ class ExcelRead():
         return sheet_name
 
 
-    def read_file(self, sheet_name):
-        df = pd.read_excel(file_path, sheet_name=sheet_name, header=None, index_col=None)
-        ''' header = None, index_col = None 避免pandas自动更改行号和列号, 使用原始索引读取 '''
-        # try:
-        #     # 找到包含'字节序号'的行号
-        #     header_index = df[df.eq('字节序号').any(axis=1)].index[0]
-        #     # 将'字节序号'作为columns
-        #     df.columns = df.iloc[header_index]
-        #     # 删除作为列名的行, 并重置索引
-        #     df = df[(header_index + 1):].reset_index(drop=True)
-        #     # print(df)
-        # except Exception as e:
-        #     print(str(e))
+    def read_file(self, sheet_name: str):
 
-        # '内容' 所在的行号和列号
-        content_row_index, content_col_index = df.eq('内容').stack().idxmax()
+        protocol = {}
+
+        df = pd.read_excel(file_path, sheet_name=sheet_name)
+
+
+
+        # 获取 字节序号 所在的列索引
+        col_index = df.columns.get_loc(df.eq('字节序号').stack().idxmax()[1])
         ''' 
-        df.eq('内容') - 返回 boolean型的DataFrame, 标记每个单元格是否是 '内容’
+        df.eq('内容') - 返回 boolean型的DataFrame, 标记每个单元格是否是 '字节序号’
         .stack()     - 将DataFrame转为一维索引的 Series
-        .idxmax()    - 找到第一个为 True 的索引
+        .idxmax()    - 找到第一个为 True 的位置, 返回行索引+列所用
         '''
 
-        # 只读取'内容'左边的字节序号列 和 右边的那一列
-        start_col_index, end_col_index = content_col_index - 1, content_col_index + 2
-        target_cells = df.iloc[content_row_index:, start_col_index: end_col_index]
+        # 选取列范围, 从 字节序号 所在的列开始 往右取两列
+        target_cells = df.iloc[1:, col_index: col_index + 3]
+
+        # 设置目标三列的 columns
+        target_cells.columns = ['字节序号', '内容', '开关描述']
+        # 填充 字节序号 列, 如果字节序号出现一次以上, 认为是开关量, 否则是模拟量
+        target_cells.loc[:, '字节序号'] = target_cells['字节序号'].fillna(method='ffill')
+        # 筛选出重复的字节序号, 作为开关量
+        duplicates = target_cells['字节序号'].value_counts()
+        repeat_values = duplicates[duplicates > 1]
+
+        # 将开关量的字典初始化
+        for digital_switch in repeat_values.index:
+            protocol[digital_switch] = {}
+
+        # 删除 开关描述 中为 nan 或 有 预留 的单元格所在行, 并保留 CRC(最后一行)
+        target_cells = target_cells.iloc[:-1][~(target_cells.iloc[:-1]['开关描述'].isna() |
+                                               target_cells.iloc[:-1]['开关描述'].astype(str).str.contains('预留'))]
+        # 添加CRC最后一行回去
+        target_cells = pd.concat([target_cells, target_cells.iloc[[-1]]], ignore_index=True)
+
+        # 更新开关量字典
 
 
-        # 删除右边那一列中包含 预留 或是 nan 的所有行
-        # target_cells = target_cells[~target_cells.iloc[:, 2].astype(str).str.contains('预留', na=False)]
-        # print(target_cells.columns)
+        print(target_cells)
 
-        # target_cells = target_cells[target_cells[4]].astype(str).str.contains('预留', na=False)
 
-        print(target_cells[1])
-        # print(target_cells)
+
 
 
 if __name__ == '__main__':
-    file_path = "D:\LearniuH\Project\中测推土机\功能定义\推土机标准化内部通信协议-V1.0-202400918.xlsx"
+    # file_path = "D:\LearniuH\Project\中测推土机\功能定义\推土机标准化内部通信协议-V1.0-202400918.xlsx"
+    file_path = "C:\\Users\L\Desktop\自动化测试\推土机标准化内部通信协议-V1.0-202400918.xlsx"
     sheet_name = '（推土机-OU）->MU&OC'
     a = ExcelRead(file_path)
     # a.read_sheet_name()
